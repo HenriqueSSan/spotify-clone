351ff847282d7bb735abe877612f740d141de332
HenriqueSSan <hsstudentwork@gmail.com>
2023-01-15 11:57:13 -0300

    feat(login): integration  token access, permission player

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,28 @@
 import './styles/index.scss';
 
+import { useEffect, useState } from 'react';
+
+import { Login } from './pages/Login';
+import { getTokenFromUrl } from './pages/Login/spotify';
+
+function Player() {
+  return <div></div>;
+}
+
 export function App() {
-  return <div className="App"></div>;
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    const hash: { access_token: string } = getTokenFromUrl();
+
+    window.location.hash = '';
+
+    const _token = hash?.access_token;
+
+    if (_token) {
+      setToken(token);
+    }
+  }, []);
+
+  return <div className="App">{token ? <Player /> : <Login />}</div>;
 }
