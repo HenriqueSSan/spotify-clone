512efed105abd251dbaa8a3161f580cae523bfe8
HenriqueSSan <hsstudentwork@gmail.com>
2023-01-15 12:32:52 -0300

    feat(login): finished login

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import './styles/index.scss';
 
 import { useEffect, useState } from 'react';
+import SpotifyWebApi from 'spotify-web-api-js';
 
 import { Login } from './pages/Login';
 import { getTokenFromUrl } from './pages/Login/spotify';
 
+const spotify = new SpotifyWebApi();
+
 function Player() {
-  return <div></div>;
+  return <div>I&apos;m Player</div>;
 }
 
 export function App() {
-  const [token, setToken] = useState(null);
+  const [token, setToken] = useState<string | null>(null);
 
   useEffect(() => {
     const hash: { access_token: string } = getTokenFromUrl();
@@ -20,7 +23,13 @@ export function App() {
     const _token = hash?.access_token;
 
     if (_token) {
-      setToken(token);
+      setToken(_token);
+
+      spotify.setAccessToken(_token);
+
+      spotify.getMe().then((user) => {
+        console.log(user);
+      });
     }
   }, []);
 
